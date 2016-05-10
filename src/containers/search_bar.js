7c2a685a989e32f83ca44882199cd2bf76ec64dd
Marck <mercercountynj@gmail.com>
2016-05-10 17:12:34 -0400

    added search_bar container and bind(this)

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+     // initialize state
+     constructor(props) {
+          super(props);
+          
+          this.state = { term: '' };
+          /* avoids: uncaught typeerror: cannot read property 'setstate' of undefined */  
+          this.onInputChange = this.onInputchange.bind(this);
+     }
+     
+     onInputChange(event) {
+          console.log(event.target.value);
+          this.setState({ term: event.target.value })
+     }
+     
      render() {
           return (
                <form className="input-group">
-                    <input />
+                    <input 
+                         placeholder="Get your five-day forecast"
+                         className="form-control"
+                         value={this.state.term}
+                         onChange={this.onInputChange} />
                     <span className="input-group-btn">
-                         <button type="submit" className="btn btn-secondary">Submit</button>
+                         <button type="submit" 
+                         className="btn btn-secondary">
+                              Submit
+                         </button>
                     </span>
                </form>
           );

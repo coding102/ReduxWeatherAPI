fa8eb57f4f17f288c2878c33870939ab2be5f1a0
Marck <mercercountynj@gmail.com>
2016-05-11 16:24:21 -0400

    added action, installed axios ajax

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,20 @@
+// http://openweathermap.org/forecast5
+// npm install --save axios ajax request
+import axios from 'axios';
+
 const API_KEY= "9ec9f24d36960b618105e52eab4ddc0f";
+const ROOT_URL= `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+// Actions have to have "type"
+export function fetchWeather(city) {
+     // http://openweathermap.org/forecast5
+     const url = `${ROOT_URL}&q=${city}, us`;
+     const request = axios.get(url);
+     
+     return {
+          type: 'FETCH_WEATHER',
+          payload: request
+     };
+}

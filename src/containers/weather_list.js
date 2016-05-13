50b834aa14b8b52349d6e718c18d52e626937c27
Marck <mercercountynj@gmail.com>
2016-05-13 16:55:38 -0400

    mapping through city names

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class WeatherList extends Component {
+     renderWeather(cityData) {
+          const name = cityData.city.name;
+          
+          return (
+               <tr key={name}>
+                    <td>{name}</td>
+               </tr>
+          );
+     }
+     
      render() {
           return (
                <table className="table table-hover">
@@ -13,10 +23,10 @@ class WeatherList extends Component {
                               <th>Humidity</th>
                          </tr>
                     
-                         </thead>
+                    </thead>
                     
-                         <tbody>
-               
+                    <tbody>
+                         {this.props.weather.map(this.renderWeather)}
                     </tbody>
                </table>
           );

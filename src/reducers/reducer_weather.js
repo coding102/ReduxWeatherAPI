905afbcc48b7806a197708580dc8bcb2f927e1f2
Marck <mercercountynj@gmail.com>
2016-05-13 00:01:25 -0400

    concat method

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,3 +1,12 @@
-export default function(state = null, action) {
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
+     switch (action.type) {
+          case FETCH_WEATHER:
+               // concat creates a new array while still containing old material
+               // we don't want to mutate state aka push method
+               // return state.concat([action.payload.data]); =
+               return [ action.payload.data, ...state ];
+     }
      return state;
 }
